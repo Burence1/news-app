3d3a5b870bfb367e807a0eea6a0226a01de0f429
Burence1 <burensomondi@gmail.com>
2021-04-16 18:25:06 +0300

    add view functions to view.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from .import main
-from ..requests import get_source
+from ..requests import get_source,get_all_articles,get_articles,get_top_headlines,search_article
 
 #Views
 @main.route('/')
@@ -14,17 +14,78 @@ def index():
   business_source = get_source('business')
   entertainment_source = get_source('entertainment')
   science_source = get_source('science')
-  sports = get_source('sports')
-  general = get_source('general')
+  sports_source = get_source('sports')
+  religion_source = get_source('religion')
 
   title = 'Best news app'
 
-  return render_template('.index' title=title,technology=technology_source)
+  search_article = request.args.get('article_query')
 
-@main.route('/articles/publishedAt')
-def articles(publishedAt):
-  articles = get_article(publishedAt)
-  title = f'{article_item.title}'
-  latest = publishedAt
+  if search_article:
+    return redirect(url_for('.search',article_name=search_article))
+  else:
+    return render_template('index.html' title=title,technology=technology_source,business=business_source,science=science_source,
+    religion=religion_source,sports=sports_source,entertainment=entertainment_source)
 
-  return render_template('.index',title=title,latest=publishedAt)
+
+@main.route('/articles/<source_id>&<int:per_page>')
+def articles(source_id,per_page):
+  '''
+  view function for returning articles
+  '''
+  article_source = get_articles(source_id,per_page)
+
+  title = f'{source_id}'
+  search_article = request.args.get('article_query')
+
+  if search_article:
+    return redirect(url_for('.search', article_name=search_article))
+
+  else:
+    return render_template('articles.html', title=title, name=source_id,articles=article_source)
+
+
+@main.route('/top_headlines&<int:per_page>')
+def top_stories(per_page):
+  '''
+  view function for top stories
+  '''
+  top_stories_articles = get_top_headlines(per_page)
+
+  title = 'Top Stories'
+
+  search_article = request.args.get('article_query')
+  if search_article:
+    return redirect(url_for('.search',article_name=search_article))
+
+  else:
+    return render_template('topstories.html',title=title,name='Top Stories',articles=top_stories_articles)
+
+
+@main.route('/all_articles&<int:per_page>')
+def all_stories(per_page):
+  '''
+  view function for all stories
+  '''
+  all_stories_articles = get_all_articles(per_page)
+  
+  title = 'All Stories'
+
+  search_article = request.args.get('article_query')
+  if search_article:
+    return redirect(url_for('.search',article_name=search_article))
+  else:
+    return render_template('allstories.html',title=title, name=All Stories,articles = all_stories_articles)
+
+
+@main.route('search_article/<article_name>')
+def search(article_name):
+  '''
+  view function that displays articles search results
+  '''
+  article_name_list = article_name.split(" ")
+  article_name_format = "+".join(article_name_list)
+  searched_article = search_article(article_name_format)
+  title = f"{article_name}'s search results"
+
+  return render_template('search.html', articles=searched_article)

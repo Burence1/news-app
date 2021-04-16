595f812d20352a85519f9ae09b00d0851916019f
Burence1 <burensomondi@gmail.com>
2021-04-16 13:10:02 +0300

    add view logic

---
+++ app/main/views.py
@@ -0,0 +1,24 @@
+from flask import render_template,request,redirect,url_for
+from .import main
+from ..requests import get_article
+
+#Views
+@main.route('/')
+def index():
+  '''
+  View root page function that returns the index page and its data
+  '''
+
+  #Getting latest articles
+  latest_articles = get_article('publishedAt')
+
+  title = 'Best news app'
+
+  return render_template('.index', publishedAt=publishedAt)
+
+@main.route('/articles/publishedAt')
+def articles(publishedAt):
+  articles = get_article(publishedAt)
+  title = f'{article_item.title}'
+
+  return render_template('.index',title=title)

ffe4191fc60aa03047c43f9d18424129c2ae0c83
Burence1 <burensomondi@gmail.com>
2021-04-18 13:00:22 +0300

    add sidebar to all html pages

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY='05507e3842b14dad8b81a6bcf1a8446d'
+export NEWS_API_KEY='0352d1de611a46e2bdfea3240936a7cc'
 export SECRET_KEY='1d6fafd5-50d7-47f9-b747-19ce8262959b'
 
 python3.6 manage.py server

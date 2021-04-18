270b62518046bfe72abead3358687a4c3f8d1086
Burence1 <burensomondi@gmail.com>
2021-04-19 00:25:18 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY='68f611fa502647cd94ab8856d93c80b3'
+export NEWS_API_KEY='082cd8af07b04c32996c8b1cae132839'
 export SECRET_KEY='1d6fafd5-50d7-47f9-b747-19ce8262959b'
 
 python3.6 manage.py server

792f6ad19484b6c77d63c7c71cd02b49bbdc2f33
Burence1 <burensomondi@gmail.com>
2021-04-19 13:48:28 +0300

    change key

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY='082cd8af07b04c32996c8b1cae132839'
+export NEWS_API_KEY='66239b3b208a4511a5736152e29abbbf'
 export SECRET_KEY='1d6fafd5-50d7-47f9-b747-19ce8262959b'
 
 python3.6 manage.py server
